43c3718631d82cbbb05e6a0d3f82be7e8c2f74d1
Jules <60539590+JulesG10@users.noreply.github.com>
2020-09-05 16:16:51 +0200

    Update app.active.js

--- app.active.js
+++ app.active.js
@@ -2,8 +2,17 @@ import {  _$ } from "./src/class/fast.active.js";
 import { components } from "./src/components/main.active.js";
 import { css } from "./src/function/css.active.js";
 import { live } from "./src/utils/utils.active.js";
+import { Active as Act,ActiveToDocument } from "./src/function/main.active.js";
 
 export var _ = ({
+    /**
+     * @function
+     * @public
+     * @name init
+     * @param {Boolean} manu
+     * @param {Object} json
+     * @returns {Promise}
+     */
     init: (manu = false, json = {}) => {
         let t = _;
         return new Promise((res, rej) => {
@@ -29,13 +38,70 @@ export var _ = ({
             }
         })
     },
+     /**
+     * @function
+     * @public
+     * @name $
+     * @param {String} el
+     * @returns {_$}
+     */
     $:(el)=>{
         return new _$(el);
     },
+    /**
+     * @function
+     * @public
+     * @name add$
+     * @param {String} name
+     * @param {Function} callback
+     * @returns {void}
+     */
     add$:(name,callback)=>{
         _$.prototype[name] = callback;
     },
+    /**
+     * @function
+     * @public
+     * @name live
+     * @param {Object} obj
+     * @param {Function} setE
+     * @returns {void}
+     */
     live:live,
+    /**
+     * @var components
+     * @name components
+     * @public
+     * @type {Object}
+     */
     components:components,
-    css:css
+    /**
+     * @var css
+     * @name css
+     * @public
+     * @type {Object}
+     */
+    css:css,
+    /**
+     * @function
+     * @public
+     * @name addActive
+     * @param {Object} obj
+     * @returns {void}
+     */
+    addActive:(obj)=>{
+        ActiveToDocument([Act(obj)])
+    }
 });
+
+/**
+* @function
+* @public
+* @name Active
+* @param {Object} obj
+* @returns {HTMLElement}
+*/
+export function Active(obj){
+    return Act(obj);
+}
+
